140914c9e9bf894b42a1670e3b4e013a2b2639a5
bwire40 <emmanuelbwire@gmail.com>
2024-08-12 20:26:19 +0300

    Modified acquisition table

--- database/migrations/2024_08_01_104324_create_acquisitions_table.php
+++ database/migrations/2024_08_01_104324_create_acquisitions_table.php
@@ -4,29 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAcquisitionsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('acquisitions', function (Blueprint $table) {
-
+            $table->id(); // primary key
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('issue_date');
+            $table->string('due_date');
+            $table->string('return_date');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('guest_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('acquisitions');
     }
-}
-
+};
